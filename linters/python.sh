970b1c404d03d1029b090db72d5c78352e9c100c
Ryan J. Price <ryapric@gmail.com>
2021-11-30 16:33:31 -0600

    Edit checks done by pylint

--- linters/python.sh
+++ linters/python.sh
@@ -4,9 +4,12 @@ set -euo pipefail
 # TODO: Find a better way to do a full directory scan for pylint, since it's mad
 # if a dir arg isn't inside a formal package
 file_list=$(find "$1" -type f -name '*.py')
+
 for f in ${file_list}; do
   printf '>>>>>>>> Running Python linter against %s...\n' "${f}"
-  pylint "${f}"
+  pylint \
+    --disable=import-error,invalid-name \
+  "${f}"
 done
 
 if [[ "${#file_list}" -gt 0 ]]; then
