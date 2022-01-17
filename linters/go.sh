8763c0dcf284239af6f9366a3ad36ca3b4d82d0a
Ryan J. Price <ryapric@gmail.com>
2022-01-16 20:15:13 -0600

    Move a check for gofmt diffs so there's no newline in every log

--- linters/go.sh
+++ linters/go.sh
@@ -4,8 +4,8 @@ set -euo pipefail
 # Fail if gofmt finds any diff from its own rules
 printf '>>>>>>>> Running Go formatter diff check...\n'
 gofmt_out=$(gofmt -d "$1")
-printf '%s\n' "${gofmt_out}"
 if [[ "${#gofmt_out}" -gt 0 ]]; then
+  printf '%s\n' "${gofmt_out}"
   exit 1
 fi
 
