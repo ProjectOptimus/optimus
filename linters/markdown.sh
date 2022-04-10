f74b9b85b70c650e443e629389ec268b85376d50
Ryan J. Price <ryapric@gmail.com>
2022-04-09 20:33:50 -0500

    Have mdl ignore terraform-docs-generated content, if it's in the right place

--- linters/markdown.sh
+++ linters/markdown.sh
@@ -5,4 +5,14 @@ set -euo pipefail
 source "${RHAD_ROOT:-}"/scripts/utils.sh
 
 printf '>>>>>>>> Running Markdown linter...\n'
-mdl --style /root/linters/.mdlrc.style.rb "$1" || mark-failed-lint 'markdown-markdownlint'
+
+# Find list of files to lint, but ignore the ones that should be... well,
+# ignored; e.g. terraform-docs-generated .md files
+find "$1" -type f -name '*.md' -or -name '*.markdown' > /tmp/rhad-mdl-files
+grep -vE '.*providers/.*/docs/README.md' /tmp/rhad-mdl-files > /tmp/rhad-mdl-files-cleaned
+
+# shellcheck disable=SC2046
+mdl \
+  --style /root/linters/.mdlrc.style.rb \
+  $(paste -s -d' ' /tmp/rhad-mdl-files-cleaned) \
+|| mark-failed-lint 'markdown-markdownlint'
